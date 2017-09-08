d93cab90ea3afb16d7cf11b43b98534dd5cb4834
Jenya Polischuk <z.polischuk@gmail.com>
2017-09-08 16:34:17 +0300

    Initial developments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FullName } from './appclass/fullname';
+import { CitizenService } from './appservice/citizen.service';
+import { Citizen } from './appclass/citizen';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CitizenService]
 })
 export class AppComponent {
-  title = 'app';
+    authorized: boolean;
+    citizen: Citizen;
+    
+    constructor() {}
+    
+    ngOnInit() {
+        this.authorized = false;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { routing }        from './app.routing';
+
+import { CitizenComponent } from './citizen/citizen.component';
+import { CommunityComponent } from './community/community.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
+import { AgmCoreModule } from '@agm/core'
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LogupService } from './appservice/logup.service';
+import { DialogComponent } from './dialog/dialog.component';
+import { MessageComponent } from './message/message.component';
+import { StompService } from 'ng2-stomp-service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitizenComponent,
+    CommunityComponent,
+    HomeComponent,
+    RegistrationComponent,
+    LoginComponent,
+    MapComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    DialogComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpModule,
+    routing,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC4_vqUuV5u6VKXeQiVxp-SYuP-qZZ_iTw',
+      libraries: ["places"]
+    })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LogupService, StompService],
+  bootstrap: [AppComponent],
+  exports: [MapComponent]
 })
 export class AppModule { }
